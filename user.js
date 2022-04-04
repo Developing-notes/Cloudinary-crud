28ccc58a24517da904712660f9ab14f1a84aad90
Tamilanworld <101121374+Tamilanworld@users.noreply.github.com>
2022-04-04 23:21:59 +0530

    routes-folder

--- user.js
+++ user.js
@@ -1,14 +1,60 @@
-const mongoose = require('mongoose')
-const userschema = new mongoose.Schema({
-    name: {
-        type: String
-    },
-    avatar: {
-        type: String
-    },
-    cloudinary_id: {
-        type: String
-    }
+const express = require('express')
+const router = express.Router();
+const cloudinary = require('../utils/cloudinary')
+const upload = require('../utils/multer')
 
+const User = require('../model/user')
+router.post('/upload', upload.single('image'), async (req, res) => {
+    try {
+        const result = await cloudinary.uploader.upload(req.file.path)
+        //create instance of user
+        let user = new User({
+            name: req.body.name,
+            avatar: result.secure_url,
+            cloudinary_id: result.public_id
+        })//save user
+        user.save()
+        res.json(user)
+    } catch (err) {
+        console.log(err)
+
+    }
+});
+router.get('/find', (req, res) => {
+    User.find(function (err, data) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.send(data);
+        }
+    });
+});
+
+router.delete("/remove", async (req, res) => {
+    try {
+        let user = await User.findById(req.body._id)
+        await cloudinary.uploader.destroy(user.cloudinary_id)//delete cloude_id
+        await user.remove()//remove database
+        res.json(user)
+    } catch (error) {
+        console.log(error)
+    }
 })
-module.exports = mongoose.model('User', userschema)
+// router.put('/:id', async (req, res) => {
+//     try {
+//         let user = await User.findById(req.params.id)
+//         await cloudinary.uploader.destroy(user.cloudinary_id)
+//         const result = await cloudinary.uploader.upload(req.file.path)
+//         const data = {
+//             name: req.body.name || user.name,
+//             avatar: result.secure_url || user.avatar,
+//             cloudinary_id: result.public_id || user.cloudinary_id
+//         }
+//         user = await User.findByIdAndUpdate(req.params.id, data, { new: true })
+//         res.json(user)
+//     } catch (error) {
+//         console.log(error)
+//     }
+// })
+module.exports = router
